c543e9d010b21699f8e85a145b090361839df6ec
Erfan Ahmed Siam <erfan.siam98@gmail.com>
2023-05-28 22:05:43 +0600

    updated

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -9,7 +9,7 @@ $art="
 
 "
 
- #echo $art
+echo $art
 
 
 
@@ -27,13 +27,33 @@ Set-PSReadLineOption -PredictionSource History
 #PS Terminal ICON
 Import-Module -Name Terminal-Icons
 
+
 Set-Alias g git
+function log{
+    git log --all --decorate --oneline --graph
+}
+function commit($message){
+    git add .;
+    git commit -m "$message"
+}
 
+#aliases for Laravel
+# ----------------------
+# Erfan Ahmed Siam
+# --------------------
 function art($arg1,$arg2,$arg3,$arg4,$arg5) { php artisan $arg1 $arg2 $arg3 $arg4 $arg5}
-
-Set-Alias a art
-Set-Alias c clear
-
-
-
-
+function serve(){
+    php artisan serve
+}
+function migrate{
+    php artisan migrate
+}
+function fresh{
+    php artisan migrate:fresh
+}
+function laragon{
+    cd C:\laragon\www
+}
+function seed{
+    php artisan db:seed
+}
